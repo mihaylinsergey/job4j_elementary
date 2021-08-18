8756d32e4b098e001615aa2d916f08dad6fd77b5
mihaylinsergey <semi999@mail.ru>
2021-08-18 22:10:51 +0300

    9.1. String [#237407]

---
+++ src/condition/Greeting.java
@@ -0,0 +1,9 @@
+package condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+    String idea="I like Java!";
+    int year=2021;
+    System.out.println(idea+" But I am a newbie "+year);
+    }
+}
