6d07a67c09c006764e9dbb79543a68585305ac34
mihaylinsergey <semi999@mail.ru>
2021-08-19 20:35:06 +0300

    9.1. String [#237407 #191552]

--- src/condition/Greeting.java
+++ src/condition/Greeting.java
@@ -3,7 +3,10 @@ package condition;
 public class Greeting {
     public static void main(String[] args) {
     String idea = "I like Java!";
+    System.out.println(idea);
+    idea += " But I am a newbie ";
     int year = 2021;
-    System.out.println(idea + " But I am a newbie " + year);
+    idea += year;
+    System.out.println(idea);
     }
 }
