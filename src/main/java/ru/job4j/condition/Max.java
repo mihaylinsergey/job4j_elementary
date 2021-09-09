972f0443aeea479e84d3e77225806d8e970b7f07
mihaylinsergey <semi999@mail.ru>
2021-09-09 21:22:47 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,8 +1,18 @@
 package ru.job4j.condition;
 
 public class Max {
-            public static int max(int left, int right) {
-            int result = left >= right ? left : right;
-            return result;
+    public static int max(int first, int second) {
+        int result = first >= second ? first : second;
+        return result;
         }
+
+    public static int max(int first, int second, int third) {
+        int result = max(first, second) >= max(second, third) ? max(first, second) : max(second, third);
+        return result;
+     }
+
+    public static int max(int first, int second, int third, int fourth) {
+        int result = fourth >= max(first, second, third) ? fourth : max(first, second, third);
+        return result;
+    }
     }
--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -6,28 +6,31 @@ import org.junit.Test;
 public class MaxTest {
     @Test
     public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        int first = 1;
+        int second = 2;
+        int result = Max.max(first, second);
         int expected = 2;
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void whenMax3To1Then3() {
-        int left = 3;
-        int right = 1;
-        int result = Max.max(left, right);
-        int expected = 3;
+    public void whenThreeNumbers() {
+        int first = 6;
+        int second = 7;
+        int third = 10;
+        int result = Max.max(first, second, third);
+        int expected = 10;
         Assert.assertEquals(result, expected);
     }
 
     @Test
-    public void whenMax1To1Then1() {
-        int left = 1;
-        int right = 1;
-        int result = Max.max(left, right);
-        int expected = 1;
+    public void whenFourNumbers() {
+        int first = -3;
+        int second = 99;
+        int third = 0;
+        int fourth = 3;
+        int result = Max.max(first, second, third, fourth);
+        int expected = 99;
         Assert.assertEquals(result, expected);
     }
 }
